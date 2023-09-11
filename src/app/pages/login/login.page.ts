fba7b43181e49514e890f0f7dceb74a7e93a9309
mayank1205 <mj.mayank1205@gmail.com>
2023-09-11 15:44:43 +0530

    add login form

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoginPageForm } from './login.page.form';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +10,16 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private router: Router) { }
+  form: FormGroup;
+
+  constructor(private router: Router, private formbuilder: FormBuilder) { }
 
   login(){
     this.router.navigate(['home'])
   }
 
   ngOnInit() {
+    this.form = new LoginPageForm(this.formbuilder).createForm();
   }
 
 }
